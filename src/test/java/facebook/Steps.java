dd53aa043188a10b958336e42ae09b854175a1a2
Abhishek Sen <abhishek.bcrec@gmail.com>
2020-04-09 16:20:45 +0530

    Screenshot html fixed

--- src/test/java/facebook/Steps.java
+++ src/test/java/facebook/Steps.java
@@ -1,12 +1,19 @@
 package facebook;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-
+import com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.*;
 import io.cucumber.java.en.*;
@@ -15,19 +22,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Steps {
 
 	private WebDriver driver;
+	static String screenshotdir = System.getProperty("user.dir") + "/test-output/screenshots/";
 
-	@BeforeStep
-	public void beforeStep() throws InterruptedException {
-		Thread.sleep(250);
+	@Before
+	public void clean_directory() throws Throwable {
+		if ((new File(screenshotdir)).exists())
+			FileUtils.cleanDirectory(new File(screenshotdir));
 	}
 
 	@AfterStep
-	public void afterStep(Scenario scenario) {
-		System.out.println("AFTER STEP");
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
-		scenario.write("step is completed and attaching screenshot.");
-		scenario.embed(screenshot, "image/png");
+	public void attach_screenshot(Scenario scenario) throws Throwable {
+		Thread.sleep(2000);
+		String imagepath = captureScreenShot(driver);
+		ExtentCucumberAdapter.addTestStepScreenCaptureFromPath(imagepath);
+		scenario.embed(Files.readAllBytes(Paths.get(imagepath)), "image/png");
 	}
 
 	@After
@@ -62,6 +70,27 @@ public class Steps {
 	public void user_login() {
 		WebElement loginbutton = driver.findElement(By.id("loginbutton"));
 		Assert.assertTrue(loginbutton.isDisplayed(), "User failed to login");
+		
+	}
+	public static String captureScreenShot(WebDriver driver) throws IOException {
+		TakesScreenshot screen = (TakesScreenshot) driver;
+		File src = screen.getScreenshotAs(OutputType.FILE);
+		String dest = screenshotdir + "img" + getcurrentdateandtime() + ".png";
+		File target = new File(dest);
+		FileUtils.copyFile(src, target);
+		return dest;
+	}
+
+	private static String getcurrentdateandtime() {
+		String str = null;
+		try {
+			DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss:SSS");
+			Date date = new Date();
+			str = dateFormat.format(date);
+			str = str.replace(" ", "").replaceAll("/", "").replaceAll(":", "");
+		} catch (Exception e) {
+		}
+		return str;
 	}
 
 }
